9ccf3c117f867d567a3cb120a7e7b919ffdcbcb1
Inesh <ineshindeewara@gmail.com>
2026-07-03 22:03:18 +0530

    Total score fix

--- iOS Tasks/Features/MainMenu/MainMenuView.swift
+++ iOS Tasks/Features/MainMenu/MainMenuView.swift
@@ -41,15 +41,15 @@ struct MainMenuView: View {
             
             Spacer()
             
-            Text("2,450 PTS")
-                .font(.system(size: 11, weight: .black))
+            Text("\(bestTapFrenzy + bestLightItUp + bestQuizRush) PTS")
+                .font(.system(size: 16, weight: .black))
                 .foregroundColor(brutalistDark)
-                .padding(.horizontal, 12)
-                .padding(.vertical, 4)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 6)
                 .background(
                     cautionYellow
                         .border(brutalistDark, width: 2)
-                        .shadow(color: brutalistDark, radius: 0, x: 2, y: 2)
+                        .shadow(color: brutalistDark, radius: 0, x: 3, y: 3)
                 )
         }
         .padding(.horizontal, 20)
