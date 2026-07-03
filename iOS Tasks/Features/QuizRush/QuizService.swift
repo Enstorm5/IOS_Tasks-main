913f3c22133cd1c3bed31f4d3c9c1d8475449caf
Inesh <ineshindeewara@gmail.com>
2026-07-03 22:26:25 +0530

    quiz question type update

--- iOS Tasks/Features/QuizRush/QuizService.swift
+++ iOS Tasks/Features/QuizRush/QuizService.swift
@@ -7,7 +7,11 @@ struct QuizService {
     }
     
     func fetchQuestions() async throws -> [Question] {
-        guard let url = URL(string: "https://opentdb.com/api.php?amount=10&type=multiple") else {
+        // Categories: 11 = Film, 12 = Music, 14 = Television, 15 = Video Games
+        let allowedCategories = [11, 12, 14, 15]
+        let randomCategory = allowedCategories.randomElement()!
+        
+        guard let url = URL(string: "https://opentdb.com/api.php?amount=10&category=\(randomCategory)&type=multiple") else {
             throw NetworkError.badURL
         }
         
